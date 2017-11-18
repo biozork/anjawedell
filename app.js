9eea787551848c834cb53abdea44fe6af0fee29f
Lars Emil Christensen <larsemil@biozork.dk>
2017-11-18 22:23:27 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var port = 3000;
+
+var requestHandler = (request, response) => {
+  console.log(request.url);
+  response.end('Hello Node.js Server!');
+}
+
+var server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
