5b0d777f530960593e7b92059b4a640524606635
biozork <larsemil@biozork.dk>
2017-11-20 21:14:44 +0100

    basic implementation
    only frontpage works at the moment

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-/* global require, console */
+/* global require */
+/* global __dirname */
+
+// Modules
 var http = require('http');
+var express = require('express');
+var serveStatic = require('serve-static');
+var app = express();
+var http = require('http').Server(app);
+
+// Enviroment variables
 var port = process.env.PORT || 3000;
 
-var requestHandler = (request, response) => {
-  console.log(request.url);
-  response.end('Hello Node.js Server! ninjanja er meeeeeega sej');
-}
+//Define view folder, and view engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-var server = http.createServer(requestHandler);
+// Serve static files from this folder - will become /
+app.use(serveStatic(__dirname + '/public'));
 
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err);
-  }
+// Routes
+var public = require('./routes/public.js');
+app.use('/', public);
 
-  console.log(`server is listening on ${port}`);
+//Start server
+http.listen(port, function () {
+    console.log('Server is now running on ' + port);
 });
